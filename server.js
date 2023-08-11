0e05d03cce1e8e9e326bbf9986bf6372f54f597f
MastourEya <eya.mastour@avaxia-group.com>
2023-08-11 12:10:26 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import colors from 'colors'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import connectDB from './config/db.js'
+import authRoutes from './routes/authRoutes.js'
+
+dotenv.config()
+
+const app = express()
+//database
+connectDB();
+app.get('/', (req,res) => {
+    res.send('<h1>Welcome to the ecommerce app !</h1>')
+})
+
+app.use(express.json())
+app.use(morgan('dev'))
+
+const PORT = process.env.PORT || 8080;
+
+//routes
+app.use("/api/v1/auth", authRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is Running on ${process.env.DEV_MODE}  mode on port ${PORT}`.bgCyan.white);
+});
